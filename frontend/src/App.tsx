fa88d91ac59509591302092726216e078e56dc4b
Lian Ulloa <lianulloa808@gmail.com>
2021-11-15 01:30:18 -0500

    UNSTABLE: set api and store scaffold

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import CssBaseline from '@mui/material/CssBaseline'
+import Container from '@mui/material/Container'
+import './App.css'
+
+function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <h1>quotes</h1>
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ frontend/src/api/blueQuote.ts
@@ -0,0 +1,17 @@
+import ApiSet from "./apiSet"
+
+interface Quote {
+  buy_price: number,
+  sell_price: number,
+  source: string,
+  createdAt?: string
+}
+
+interface GroupedQuote {
+  _id: string,
+  quotes: Omit<Quote, "source">[]
+}
+
+const quoteApi = new ApiSet<Quote|GroupedQuote>("quotes")
+
+export default quoteApi
